a240ea37f9bf6b594d2abb84432b4714cab81838
Hasan-Rifat <developerhr000@gmail.com>
2023-11-13 20:52:50 +0600

    [header complete]

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-function toggleMenu() {
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-    hamburger.style.display = "block";
-    close.style.display = "none";
-  } else {
-    menu.style.display = "block";
-    hamburger.style.display = "none";
-    close.style.display = "block";
-  }
-}
+const menu_btn = document.getElementById("menu_btn");
+const mobile_menu = document.getElementById("mobile_menu");
+const menu_bars = document.getElementById("menu_bars");
+const menu_close = document.getElementById("menu_close");
+
+menu_btn.addEventListener("click", () => {
+  mobile_menu.classList.toggle("hidden");
+  menu_bars.classList.toggle("hidden");
+  menu_close.classList.toggle("hidden");
+});
